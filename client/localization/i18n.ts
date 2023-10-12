ce4d34714cdf3c7ca757194a120b0cbdcb68611c
Dmitry Rafikov <vladulasov366@gmail.com>
2023-10-12 09:34:55 +0600

    added localization & npm packages

--- client/localization/i18n.ts
+++ client/localization/i18n.ts
@@ -1,11 +1,17 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
-import translationEN from './en/translation.json'
+import en from './translations/auth/en.json'
+import ru from './translations/auth/ru.json'
+import enFormat from './formats/enFormats.json'
+import dayjs from "dayjs";
 
 export const resources = {
     en: {
-        translation: translationEN,
+        translation: en, enFormat
     },
+    ru:{
+        translation: ru,
+    }
 }
 
 i18n.use(initReactI18next).init({
@@ -13,6 +19,25 @@ i18n.use(initReactI18next).init({
     lng: 'en',
     fallbackLng: 'en',
     interpolation: {
-        escapeValue: false,
-    },
+        format: function (value: any, format: string, lng: string) {
+            if (value instanceof Date) {
+                return formatDate(value, format)
+            }
+            switch (format) {
+                case 'currency':
+                    return formatCurrency(value, lng)
+            }
+            return value;
+        },
+    }
 })
+
+function formatDate(date: Date, format?: string) {
+    return dayjs(date).format(format)
+}
+function formatCurrency(value: number, lng?: string) {
+    return Intl.NumberFormat(lng, {
+        style: 'currency',
+        currency: 'EUR',
+    }).format(value)
+}
